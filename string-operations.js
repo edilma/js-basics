6200335975107bba3ecc89f03fc896480066fc28
Edilma <edilma@Damians-M1.local>
2023-01-11 16:55:12 -0500

    strings exercises

---
+++ string-operations.js
@@ -0,0 +1,32 @@
+const myName = "Edilma";
+const faveColor = "Green";
+
+// create the following sentence
+//const fullSentence = " Hello, " + myName + ".  Your favorite fav is " + faveColor;
+const fullSentence =`Hello ${myName}. Your favorite color is ${faveColor}`;
+
+//console.log("contact => ", fullSentence);
+//console.log("string length ", fullSentence.length);
+//console.log ("To upper case ", fullSentence.toUpperCase());
+
+//console.log("To lowercase ", fullSentence.toLowerCase());
+//console.log("trim ", fullSentence.trim());
+//const newSentence = fullSentence.replace ("Hello", "Yo")
+//console.log(newSentence);
+
+// let othersentence ="Integer";
+// //console.log(othersentence.substring(0,3));
+// const newtest = othersentence.startsWith("false");
+// console.log(newtest);
+
+// const oneMoreSentence = "Hi Danny, you are not Tony"; 
+// const searchTerm = "Mack";
+
+// const result = oneMoreSentence.includes(searchTerm);
+// console.log(result);
+const newWord = "I like Papaya"
+
+const searchTerm = "love";
+const result= newWord.indexOf(searchTerm);
+
+console.log(result);
