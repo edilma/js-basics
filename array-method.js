47c53823602dd85d98bbcf70ec8c86bfe3e95319
Edilma <edilma@Damians-M1.local>
2023-01-11 15:59:24 -0500

    Working with arrays

---
+++ array-method.js
@@ -0,0 +1,19 @@
+const cuisine =[
+    "CH",
+    "IN",
+    "JA",
+    "KR",
+]
+
+// console.log("array => ", cuisine);
+// cuisine.push("MX");
+
+// console.log("array => ", cuisine);
+// // cuisine.pop();
+// cuisine.shift();
+// console.log("after shift =>" , cuisine);
+// cuisine.unshift("IT");
+// console.log("after unshift => ", cuisine);
+
+cuisine[20] = "Israeli Food";
+console.log(cuisine);
---
+++ array-multi-dimension.js
@@ -0,0 +1,21 @@
+const numberArray =["value1",//0
+                "value2", //1
+                "value3",//2
+                [
+                    "sub-value1",
+                     "sub-value2",//3
+                     "sub-value3"], 
+                        ["sub-valueA",//4
+                         "sub-valueB",
+                         "sub-valueC"],
+                         {
+                            name: "Edilma",//5
+
+                            gender: "Female"
+                         }]
+
+
+
+
+console.log(numberArray[2]);
+console.log(numberArray[5]["name"]);
